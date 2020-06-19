2d0105fd3bbd8c94c6093dd6fa157a5f1e63b3a1
xjh <1023004263@qq.com>
2020-06-19 17:05:45 +0800

    添加了一小段代码

--- test/src/main/java/hh/TestController.java
+++ test/src/main/java/hh/TestController.java
@@ -2,6 +2,7 @@ package hh;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,5 +19,43 @@ public class TestController {
     @ResponseBody
     public String toT(ModelAndView view) {
         return  "hello";
+    }
+
+    @GetMapping("/judge")
+    @ResponseBody
+    public String judge(ModelAndView view,
+                        @RequestParam(value = "string",required = false,defaultValue = " ") String s) {
+        System.out.println(s);
+        if(this.backspaceCompare(s,"abc")){
+
+            return "和abc相同";
+        }else{
+            return "不等于abc";
+        }
+    }
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
+        }
+        return true;
     }
 }
